af64ec6292ebdf231cab758c054f76011f562102
Shikhar Tiwari <trenu068@gmail.com>
2026-01-20 18:13:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+region = "ap-south-1"
+}
+
+resource "aws_instane" "myserver" {
+count =1
+ami = "ami-02b8269d5e85954ef"
+instance_type = "t3.micro"
+tags = {
+Name = "aviz-tf-server"
+}
